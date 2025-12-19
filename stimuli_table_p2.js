cc20d50aa083a811d3cce6cc4cab777d5e69de92
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2025-12-19 16:22:06 +0100

    Add extra stimuli

---
+++ stimuli_table_p2.js
@@ -0,0 +1,292 @@
+let p2_prac_items = [
+    {
+        type: "practice",
+        id: "practice_a",
+        img: "practice_a_0.png",
+        word: "nentox",
+        click: "practice_a_click.wav",
+        space_resp: "practice_a_1st.wav",
+        r_resp: "practice_a_2nd.wav"
+    },
+    {
+        type: "practice",
+        id: "practice_b",
+        img: "practice_b_0.png",
+        word: "nonfex",
+        click: "practice_b_click.wav",
+        space_resp: "practice_b_1st.wav",
+        r_resp: "practice_b_2nd.wav"
+    }
+]
+let p2_test_items = [
+    {
+        type: "vowel1",
+        id: "deatec_a",
+        img: "deatec_a_0.png",
+        word: "deatec",
+        click: "deatec_a_click.wav",
+        space_resp: "deatec_a_1st.wav",
+        r_resp: "deatec_a_2nd.wav"
+    },
+    {
+        type: "vowel1",
+        id: "deatec_b",
+        img: "deatec_b_0.png",
+        word: "deatec",
+        click: "deatec_b_click.wav",
+        space_resp: "deatec_b_1st.wav",
+        r_resp: "deatec_b_2nd.wav"
+    },
+    {
+        type: "vowel1",
+        id: "seapec_a",
+        img: "seapec_a_0.png",
+        word: "seapec",
+        click: "seapec_a_click.wav",
+        space_resp: "seapec_a_1st.wav",
+        r_resp: "seapec_a_2nd.wav"
+    },
+    {
+        type: "vowel1",
+        id: "seapec_b",
+        img: "seapec_b_0.png",
+        word: "seapec",
+        click: "seapec_b_click.wav",
+        space_resp: "seapec_b_1st.wav",
+        r_resp: "seapec_b_2nd.wav"
+    },
+    {
+        type: "vowel1",
+        id: "teakec_a",
+        img: "teakec_a_0.png",
+        word: "teakec",
+        click: "teakec_a_click.wav",
+        space_resp: "teakec_a_1st.wav",
+        r_resp: "teakec_a_2nd.wav"
+    },
+    {
+        type: "vowel1",
+        id: "teakec_b",
+        img: "teakec_b_0.png",
+        word: "teakec",
+        click: "teakec_b_click.wav",
+        space_resp: "teakec_b_1st.wav",
+        r_resp: "teakec_b_2nd.wav"
+    },
+    {
+        type: "vowel1",
+        id: "zeatec_a",
+        img: "zeatec_a_0.png",
+        word: "zeatec",
+        click: "zeatec_a_click.wav",
+        space_resp: "zeatec_a_1st.wav",
+        r_resp: "zeatec_a_2nd.wav"
+    },
+    {
+        type: "vowel1",
+        id: "zeatec_b",
+        img: "zeatec_b_0.png",
+        word: "zeatec",
+        click: "zeatec_b_click.wav",
+        space_resp: "zeatec_b_1st.wav",
+        r_resp: "zeatec_b_2nd.wav"
+    },
+    {
+        type: "vowel2",
+        id: "gookec_a",
+        img: "gookec_a_0.png",
+        word: "gookec",
+        click: "gookec_a_click.wav",
+        space_resp: "gookec_a_1st.wav",
+        r_resp: "gookec_a_2nd.wav"
+    },
+    {
+        type: "vowel2",
+        id: "gookec_b",
+        img: "gookec_b_0.png",
+        word: "gookec",
+        click: "gookec_b_click.wav",
+        space_resp: "gookec_b_1st.wav",
+        r_resp: "gookec_b_2nd.wav"
+    },
+    {
+        type: "vowel2",
+        id: "goopec_a",
+        img: "goopec_a_0.png",
+        word: "goopec",
+        click: "goopec_a_click.wav",
+        space_resp: "goopec_a_1st.wav",
+        r_resp: "goopec_a_2nd.wav"
+    },
+    {
+        type: "vowel2",
+        id: "goopec_b",
+        img: "goopec_b_0.png",
+        word: "goopec",
+        click: "goopec_b_click.wav",
+        space_resp: "goopec_b_1st.wav",
+        r_resp: "goopec_b_2nd.wav"
+    },
+    {
+        type: "vowel2",
+        id: "koosec_a",
+        img: "koosec_a_0.png",
+        word: "koosec",
+        click: "koosec_a_click.wav",
+        space_resp: "koosec_a_1st.wav",
+        r_resp: "koosec_a_2nd.wav"
+    },
+    {
+        type: "vowel2",
+        id: "koosec_b",
+        img: "koosec_b_0.png",
+        word: "koosec",
+        click: "koosec_b_click.wav",
+        space_resp: "koosec_b_1st.wav",
+        r_resp: "koosec_b_2nd.wav"
+    },
+    {
+        type: "vowel2",
+        id: "kooshec_a",
+        img: "kooshec_a_0.png",
+        word: "kooshec",
+        click: "kooshec_a_click.wav",
+        space_resp: "kooshec_a_1st.wav",
+        r_resp: "kooshec_a_2nd.wav"
+    },
+    {
+        type: "vowel2",
+        id: "kooshec_b",
+        img: "kooshec_b_0.png",
+        word: "kooshec",
+        click: "kooshec_b_click.wav",
+        space_resp: "kooshec_b_1st.wav",
+        r_resp: "kooshec_b_2nd.wav"
+    },
+    {
+        type: "vowel3",
+        id: "hakec_a",
+        img: "hakec_a_0.png",
+        word: "hakec",
+        click: "hakec_a_click.wav",
+        space_resp: "hakec_a_1st.wav",
+        r_resp: "hakec_a_2nd.wav"
+    },
+    {
+        type: "vowel3",
+        id: "hakec_b",
+        img: "hakec_b_0.png",
+        word: "hakec",
+        click: "hakec_b_click.wav",
+        space_resp: "hakec_b_1st.wav",
+        r_resp: "hakec_b_2nd.wav"
+    },
+    {
+        type: "vowel3",
+        id: "hapec_a",
+        img: "hapec_a_0.png",
+        word: "hapec",
+        click: "hapec_a_click.wav",
+        space_resp: "hapec_a_1st.wav",
+        r_resp: "hapec_a_2nd.wav"
+    },
+    {
+        type: "vowel3",
+        id: "hapec_b",
+        img: "hapec_b_0.png",
+        word: "hapec",
+        click: "hapec_b_click.wav",
+        space_resp: "hapec_b_1st.wav",
+        r_resp: "hapec_b_2nd.wav"
+    },
+    {
+        type: "vowel3",
+        id: "hashec_a",
+        img: "hashec_a_0.png",
+        word: "hashec",
+        click: "hashec_a_click.wav",
+        space_resp: "hashec_a_1st.wav",
+        r_resp: "hashec_a_2nd.wav"
+    },
+    {
+        type: "vowel3",
+        id: "hashec_b",
+        img: "hashec_b_0.png",
+        word: "hashec",
+        click: "hashec_b_click.wav",
+        space_resp: "hashec_b_1st.wav",
+        r_resp: "hashec_b_2nd.wav"
+    },
+    {
+        type: "vowel3",
+        id: "hathec_a",
+        img: "hathec_a_0.png",
+        word: "hathec",
+        click: "hathec_a_click.wav",
+        space_resp: "hathec_a_1st.wav",
+        r_resp: "hathec_a_2nd.wav"
+    },
+    {
+        type: "vowel3",
+        id: "hathec_b",
+        img: "hathec_b_0.png",
+        word: "hathec",
+        click: "hathec_b_click.wav",
+        space_resp: "hathec_b_1st.wav",
+        r_resp: "hathec_b_2nd.wav"
+    },
+    {
+        type: "filler",
+        id: "filler_a",
+        img: "filler_a_0.png",
+        word: "mantox",
+        click: "filler_a_click.wav",
+        space_resp: "filler_a_1st.wav",
+        r_resp: "filler_a_2nd.wav"
+    },
+    {
+        type: "filler",
+        id: "filler_b",
+        img: "filler_b_0.png",
+        word: "mandul",
+        click: "filler_b_click.wav",
+        space_resp: "filler_b_1st.wav",
+        r_resp: "filler_b_2nd.wav"
+    },
+    {
+        type: "filler",
+        id: "filler_c",
+        img: "filler_c_0.png",
+        word: "vonur",
+        click: "filler_c_click.wav",
+        space_resp: "filler_c_1st.wav",
+        r_resp: "filler_c_2nd.wav"
+    },
+    {
+        type: "filler",
+        id: "filler_d",
+        img: "filler_d_0.png",
+        word: "vonert",
+        click: "filler_d_click.wav",
+        space_resp: "filler_d_1st.wav",
+        r_resp: "filler_d_2nd.wav"
+    },
+    {
+        type: "filler",
+        id: "filler_e",
+        img: "filler_e_0.png",
+        word: "bomaic",
+        click: "filler_e_click.wav",
+        space_resp: "filler_e_1st.wav",
+        r_resp: "filler_e_2nd.wav"
+    },
+    {
+        type: "filler",
+        id: "filler_f",
+        img: "filler_f_0.png",
+        word: "bomes",
+        click: "filler_f_click.wav",
+        space_resp: "filler_f_1st.wav",
+        r_resp: "filler_f_2nd.wav"
+    }
+]
